d5fcc98a607c9f35a6d8211f4ccbbe3969f3a5cb
Ling Xie <shinrain@Lings-MacBook-Pro.local>
2013-08-02 01:00:30 -0700

    LAZY: test verbose level

--- code/tools/cpp/Logger/test/test.cpp
+++ code/tools/cpp/Logger/test/test.cpp
@@ -8,23 +8,22 @@ using namespace easyloggingpp;
 
 void run(void) {
     Configurations c;
-    c.setAll(ConfigurationType::Format, "[%logger] %level: %log");
-    c.setAll(ConfigurationType::Filename, "/tmp/logs/custom.log");
-    // Set default configuration for any future logger - existing logger will not use this configuration unless
-    // either true is passed in second argument or set explicitly using Loggers::reconfigureAllLoggers(c);
+    c.setAll(ConfigurationType::Filename,"logs/test.log");
+    c.setAll(ConfigurationType::Format, "%datetime [%level]: %log");
+    c.setAll(ConfigurationType::ToStandardOutput,"false");
     Loggers::setDefaultConfigurations(c);
-    LOG(INFO) << "Set default configuration but existing loggers not updated yet"; // Logging using trivial logger
     Loggers::getLogger("testDefaultConf");
-    CLOG(INFO, "testDefaultConf") << "Logging using new logger 1"; // You can also use CINFO << "..."
+    
     // Now setting default and also resetting existing loggers
-    Loggers::setDefaultConfigurations(c, true);
-    LOG(INFO) << "Existing loggers updated as well";
+    CVLOG(1,"testDefaultConf") <<"Logging using level = 1";
+	LOG(INFO) << "Existing loggers updated as well";
     Loggers::getLogger("testDefaultConf2");
     CLOG(INFO, "testDefaultConf2") << "Logging using new logger 2";
 }
 
-int main(void) {
+int main(int argc, char** argv) {
     
+    _START_EASYLOGGINGPP(argc, argv);
     run();
 
     return 0;
